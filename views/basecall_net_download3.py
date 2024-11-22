ce0b9b9c17195a9ae655ab08d22ee12a22409c84
LiuMason <yuliu2005ca@gmail.com>
2024-11-22 11:05:35 -0500

    Add files via upload

---
+++ views/basecall_net_download3.py
@@ -0,0 +1,118 @@
+
+import streamlit as st
+import pandas as pd
+import networkx as nx
+
+def upload_file():
+    uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
+    if uploaded_file:
+        df = pd.read_excel(uploaded_file,  dtype={"Charge State Distribution": str})
+        df.dropna(inplace=True)
+        st.write("Data Preview:", df.head())
+        return df
+    return None
+
+def upload_mass():
+    uploaded_file = st.sidebar.file_uploader("Choose a file", type=["csv", "xlsx", "txt"])
+    if uploaded_file is not None:
+        if uploaded_file.type == "text/csv":
+            df = pd.read_csv(uploaded_file)
+        elif uploaded_file.type == "application/vnd.ms-excel" or uploaded_file.type == "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+            df = pd.read_excel(uploaded_file)
+        elif uploaded_file.type == "text/plain":
+            df = pd.read_csv(uploaded_file, sep='\t')
+        else:
+            st.write("Unsupported file type")
+            df = None
+        
+        return dict(df.values)
+    return None
+
+def filter_data(df, mass_cutoff, time_diff_cutoff):
+    filtered_df = df[(df["Monoisotopic Mass"] >= mass_cutoff) & 
+                     ((df["Stop Time (min)"] - df["Start Time (min)"]) <= time_diff_cutoff)]
+    st.write("Filtered Data:", filtered_df)
+    return filtered_df
+
+def generate_network(filtered_df, mass_table, similarity_cutoff):
+    G = nx.DiGraph()
+    masses = filtered_df["Monoisotopic Mass"].tolist()
+    masses = sorted(masses) 
+    for i in range(len(masses)):
+        m1 = int(masses[i]) 
+        for j in range(i + 1, len(masses)):
+            mass_diff = abs(masses[j] - masses[i]) 
+            for key, value in mass_table.items():
+                m2 = int(masses[j])
+                if abs(mass_diff - value) < similarity_cutoff:
+                    G.add_node(f"M_{m1}")
+                    G.add_node(f"M_{m2}")
+                    G.add_edge(f"M_{m1}", f"M_{m2}", attribute=key)
+    st.write(mass_table) 
+    st.write("Network Generated with Nodes and Edges")
+    st.write(f"Number of nodes: {G.number_of_nodes()}")
+    st.write(f"Number of edges: {G.number_of_edges()}")
+    return G
+
+# display and download
+def display_edges(G):
+    edges = G.edges(data=True)
+    edge_list = []
+    for edge in edges:
+        edge_list.append([edge[0], 'massdiff', edge[1], edge[2]['attribute']])
+        edges_df = pd.DataFrame(edge_list, columns=["Node1","edge", "Node2", "Attribute"])
+    st.write(edges_df)
+    return edges_df
+
+@st.cache_data
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv(  sep='\t', index=False).encode("utf-8")
+
+def run_process(df, mass_cutoff, time_diff_cutoff, mass_table, similarity_cutoff):
+    filtered_df = filter_data(df, mass_cutoff, time_diff_cutoff)
+    with st.spinner('Wait for it...'):
+        G = generate_network(filtered_df, mass_table, similarity_cutoff)
+        edges = display_edges(G)
+        csv = convert_df(edges)
+        st.download_button(label="Download the network",data=csv,file_name="network.txt",mime="text/csv")
+
+
+def main():
+    st.title("Generation of MassDiff Network")
+    mass_table = {"C": 305.04129, "U": 306.0253,  "A": 329.05252, "G": 345.04743} 
+    common_mod = {"D": 308.04095, "mA":343.06817, "mC":319.05694, "mG":359.06308, "mU":320.04095} #"m22G":373.0787, "CxAA":1044.11, "ACxU":1021.09}
+    df = upload_file()
+    if df is not None:
+        st.sidebar.header("Filter Options")
+        mass_cutoff = st.sidebar.number_input("Monoisotopic Mass Cutoff", min_value=0.0, value=1500.0)
+        time_diff_cutoff = st.sidebar.number_input("Time Difference Cutoff", min_value=0.0, value=3.0)
+        similarity_cutoff = st.sidebar.number_input("Similarity Cutoff for Mass Difference", min_value=0.0, value=0.03)
+
+        st.sidebar.header("Mass Table Options")
+        if st.sidebar.button("Four nucleotides only & generate network"):
+#            with st.spinner('Wait for it...'):
+            run_process(df, mass_cutoff, time_diff_cutoff, mass_table, similarity_cutoff)
+
+        if st.sidebar.button("Four nucleotides & most common modification & generate network"):
+            mass_table.update(common_mod)
+            run_process(df, mass_cutoff, time_diff_cutoff, mass_table, similarity_cutoff)
+
+        st.sidebar.header("Add additional mass to Mass Table (common modification included)")
+        new_key = st.sidebar.text_input("New Key")
+        new_value = st.sidebar.number_input("New Value", min_value=0.0, value=0.0)
+        if st.sidebar.button("Add new mass to Mass Table & generate network"):
+            if new_key and new_value:
+                mass_table.update(common_mod)
+                mass_table[new_key] = float(new_value)
+                run_process(df, mass_cutoff, time_diff_cutoff, mass_table, similarity_cutoff)
+
+        st.sidebar.header("upload mass table (header:key,mass) & generate network")
+        mass_table = upload_mass() 
+        if mass_table is not None:
+            run_process(df, mass_cutoff, time_diff_cutoff, mass_table, similarity_cutoff)
+            
+
+#if __name__ == "__main__":
+main()
+
