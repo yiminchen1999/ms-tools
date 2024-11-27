f6f294daed8a264c2142ae765efab3e7ead1c791
LiuMason <yuliu2005ca@gmail.com>
2024-11-27 09:37:16 -0500

    Update ms_tools.py

--- ms_tools.py
+++ ms_tools.py
@@ -12,7 +12,7 @@ tool_1_page = st.Page(page = "views/ms_viz_app.py",title = "data visualization" 
 
 tool_2_page = st.Page(page = "views/ms_match_app.py", title = "ms match")
 
-tool_3_page = st.Page(page = "views/basecall_net_download3.py", title = "net generation")
+tool_3_page = st.Page(page = "views/basecall_net_download4.py", title = "net generation")
 
 pg = st.navigation( 
     { "Info":  [about_page], "Tools": [tool_1_page,tool_2_page,tool_3_page],})
