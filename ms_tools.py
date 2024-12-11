0b82e83217d248ba952a6feb3a40ff7ee1618616
LiuMason <yuliu2005ca@gmail.com>
2024-12-11 15:40:40 -0500

    Update ms_tools.py

--- ms_tools.py
+++ ms_tools.py
@@ -1,22 +1,27 @@
-
 import streamlit as st
 
 # --- PAGE SETUP ---
-about_page = st.Page(
-    page  ='views/about_tools.py',
-    title = "About tools",
-    default = True
-)
+about_page = st.Page(page  ='views/about_tools.py',title = "About tools", default = True)
 
-tool_1_page = st.Page(page = "views/ms_viz_app.py",title = "data visualization" )
+viz_page = st.Page(page = "views/viz_tool/ms_viz_tab.py",title = "MS data plots" )
+frag_viz_page= st.Page(page = "views/viz_tool/fragment_viz.py", title = "fragment plots" )
 
-tool_2_page = st.Page(page = "views/ms_match_app.py", title = "ms match")
+intact_page = st.Page(page = "views/intact_RNA/ms_match_app.py", title = "mass match")
+homology_search = st.Page(page = "views/intact_RNA/homology_search.py", title = "homology search")
 
-tool_3_page = st.Page(page = "views/basecall_net_download4.py", title = "net generation")
+net_page = st.Page(page = "views/hydrolytic_RNA/basecall_net_download4.py", title = "net generation")
+frag_page = st.Page(page = "views/hydrolytic_RNA/st_link_app.py", title = "find fragments")
+
+seq_aln_page = st.Page(page = "views/seq_tools/seq_aln.py", title = "align sequences")
+seq_rev_page = st.Page(page = "views/seq_tools/seq_rev.py", title = "reverse sequences")
 
 pg = st.navigation( 
-    { "Info":  [about_page], "Tools": [tool_1_page,tool_2_page,tool_3_page],})
+    { "Info":  [about_page], 
+      "Data Visualization": [viz_page, frag_viz_page],
+      "Tools for Intact RNA":[intact_page, homology_search], 
+      "Tools for Hydrolytic RNA" :[net_page, frag_page],
+      "Sequence Tools": [seq_aln_page, seq_rev_page],
+})
 
 pg.run()
-
 
