d9122e831552d797c0f26f0f0a64770d5ce805d0
LiuMason <yuliu2005ca@gmail.com>
2024-12-27 17:33:44 -0500

    Update ms_tools.py

--- ms_tools.py
+++ ms_tools.py
@@ -9,7 +9,7 @@ frag_viz_page= st.Page(page = "views/viz_tool/fragment_viz.py", title = "fragmen
 intact_page = st.Page(page = "views/intact_RNA/ms_match_app.py", title = "mass match")
 homology_search = st.Page(page = "views/intact_RNA/homology_search.py", title = "homology search")
 
-net_page = st.Page(page = "views/hydrolytic_RNA/basecall_net_download4.py", title = "net generation")
+net_page = st.Page(page = "views/hydrolytic_RNA/basecall_net_download5.py", title = "net generation")
 frag_page = st.Page(page = "views/hydrolytic_RNA/st_link_app.py", title = "find fragments")
 
 seq_aln_page = st.Page(page = "views/seq_tools/seq_aln.py", title = "align sequences")
