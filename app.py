c113e6036fe7f39271eb817b577201f1e68a21bd
yiminchen1999 <90345634+yimin1019@users.noreply.github.com>
2025-04-30 17:39:12 -0400

    link update

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
-
 import streamlit as st
+
 # --- PAGE SETUP ---
-about_page = st.Page(page  ='views/about_tools.py',title = "About tools")
+# Create pages
+about_page = st.Page("views/about_tools.py", title="About tools")
+viz_page = st.Page("views/viz_tool/ms_viz_tab.py", title="MS data plots")
+frag_viz_page = st.Page("views/viz_tool/fragment_viz.py", title="fragment plots")
+intact_page = st.Page("views/intact_RNA/ms_match_app.py", title="mass match")
+homology_search = st.Page("views/intact_RNA/homology_search.py", title="homology search")
+net_page = st.Page("views/hydrolytic_RNA/basecall_net_download5.py", title="net generation")
+frag_page = st.Page("views/hydrolytic_RNA/st_link_app.py", title="find fragments")
+seq_aln_page = st.Page("views/seq_tools/seq_aln.py", title="align sequences")
+seq_rev_page = st.Page("views/seq_tools/seq_rev.py", title="reverse sequences")
 
-viz_page = st.Page(page = "views/viz_tool/ms_viz_tab.py",title = "MS data plots" )
-frag_viz_page= st.Page(page = "views/viz_tool/fragment_viz.py", title = "fragment plots" )
-
-intact_page = st.Page(page = "views/intact_RNA/ms_match_app.py", title = "mass match")
-homology_search = st.Page(page = "views/intact_RNA/homology_search.py", title = "homology search")
-
-net_page = st.Page(page = "views/hydrolytic_RNA/basecall_net_download5.py", title = "net generation")
-frag_page = st.Page(page = "views/hydrolytic_RNA/st_link_app.py", title = "find fragments")
-
-seq_aln_page = st.Page(page = "views/seq_tools/seq_aln.py", title = "align sequences")
-seq_rev_page = st.Page(page = "views/seq_tools/seq_rev.py", title = "reverse sequences")
-
-pg = st.navigation(
-    { "Info":  [about_page],
-      "Data Visualization": [viz_page, frag_viz_page],
-      "Tools for Intact RNA":[intact_page, homology_search],
-      "Tools for Hydrolytic RNA" :[net_page, frag_page],
-      "Sequence Tools": [seq_aln_page, seq_rev_page],
+# Create navigation structure
+pg = st.navigation({
+    "Info": [about_page],
+    "Data Visualization": [viz_page, frag_viz_page],
+    "Tools for Intact RNA": [intact_page, homology_search],
+    "Tools for Hydrolytic RNA": [net_page, frag_page],
+    "Sequence Tools": [seq_aln_page, seq_rev_page]
 })
 
+# Run the selected page
 pg.run()
-
